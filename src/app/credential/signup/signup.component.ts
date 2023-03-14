dff818c2c8fb6ad855e7a5c824f64a41e3c18e81
144bharat <144bharatgautam@gmail.com>
2023-03-14 11:12:56 +0530

    day 14-mar-23

--- src/app/credential/signup/signup.component.ts
+++ src/app/credential/signup/signup.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormControl,FormGroup,FormControlName } from "@angular/forms";
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-
+signupform = new FormGroup(
+  {
+    email: new FormControl(''),
+    password: new FormControl(''),
+    profile: new FormControl(''),
+    cnfmpassword: new FormControl(''),
+  }
+  )
+  signupuser()
+  {
+    console.log(this.signupform.value);
+  }
 }
